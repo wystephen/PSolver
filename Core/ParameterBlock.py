de36b0e8ec62a51d1fc8adb0d13014ea3aff51ea
steve <551619855@qq.com>
2019-06-13 15:07:28 +0800

    Modified Solver..

--- Core/ParameterBlock.py
+++ Core/ParameterBlock.py
@@ -3,15 +3,15 @@ import scipy as sp
 
 
 class ParameterBlockBase:
-    def __init__(self, local_size:int,
-                 global_size:int,
-                 initial_value:np.ndarray=np.zeros(1)):
+    def __init__(self, local_size: int,
+                 global_size: int,
+                 initial_value: np.ndarray = np.zeros(1)):
         self.local_size = local_size
         self.global_size = global_size
-        if
         self.value = initial_value
 
-    def Plus(self, p_v:np.ndarray)->np.ndarray:
 
+    def Plus(self, p_v: np.ndarray) -> np.ndarray:
+        self.value += p_v
 
-
+    def
--- Core/ResidualBlock.py
+++ Core/ResidualBlock.py
@@ -5,4 +5,19 @@ import scipy as sp
 from Core.ParameterBlock import *
 
 class ResidualBlock:
-    def __init__(self):
+    def __init__(self, func_dim:int =0,
+                 para_name_list:list = list()):
+        self.func_dim:int = func_dim
+        self.func_index:int = -1
+
+        self.para_name_list = para_name_list #type List[str]
+
+    def cal_func(self,
+                 val_list:list())->np.ndarray:
+        return NotImplementedError
+
+    def cal_jac(self,
+                val_list:list)->np.ndarray:
+        return NotImplementedError
+
+
